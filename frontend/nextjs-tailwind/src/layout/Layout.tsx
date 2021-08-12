11b0e25dba60247f35ee960a20a52d916e4e63dd
Alex Patterson <alex@ajonp.com>
2021-08-12 10:04:12 -0400

    fix removal of appMenu

--- frontend/nextjs-tailwind/src/layout/Layout.tsx
+++ frontend/nextjs-tailwind/src/layout/Layout.tsx
@@ -1,22 +1,17 @@
 import { AppTopbar } from '@/layout/AppTopbar';
-import AppMenu from '@/layout/AppMenu';
 import Footer from '@/layout/Footer';
 
-const Layout = ({children}:{children: any}): JSX.Element => {
- 
-
+const Layout = ({ children }: { children: any }): JSX.Element => {
   return (
     <>
-        <AppTopbar
-        />
-        <div className="grid grid-cols-1 justify-items-center calc-height-wrapper lg:mx-auto lg:w-80 lg:max-w-8xl lg:justify-items-stretch">
-          <main className="grid justify-center w-full grid-cols-1 gap-10 bg-primary-50 dark:bg-basics-700">
-            {children}
-          </main>
+      <AppTopbar />
+      <div className="grid grid-cols-1 justify-items-center calc-height-wrapper lg:mx-auto lg:w-80 lg:max-w-8xl lg:justify-items-stretch">
+        <main className="grid justify-center w-full grid-cols-1 gap-10 bg-primary-50 dark:bg-basics-700">
+          {children}
+        </main>
 
-          <Footer  />
-        </div>
-
+        <Footer />
+      </div>
     </>
   );
 };
