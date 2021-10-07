fbe301640153ce12139fc592a88e3e5d9688f9e8
Alex Patterson <alex@ajonp.com>
2021-10-07 14:10:29 -0400

    class up the cat card some

--- frontend/nextjs-tailwind/src/components/CatCard.tsx
+++ frontend/nextjs-tailwind/src/components/CatCard.tsx
@@ -1,6 +1,15 @@
 import { Cat } from '@/models/cat.model';
 
 const CatCard = ({ cat }: { cat: Cat }): JSX.Element => {
+  const getColor = (color: string) => {
+    const o: { backgroundColor: string; color?: string } = {
+      backgroundColor: color,
+    };
+    if (color == 'white') {
+      o.color = 'black';
+    }
+    return o;
+  };
   return (
     <>
       <div className="p-2 rounded bg-primary-500 text-basics-50">
@@ -9,13 +18,21 @@ const CatCard = ({ cat }: { cat: Cat }): JSX.Element => {
             <h2>Name: {cat?.name}</h2>
             {cat.type && <p>Type: {cat?.type}</p>}
             {cat.colors && (
-              <ul className="grid grid-flow-col p-2 rounded bg-secondary-500 justify-items-center">
-                {cat?.colors?.map((color) => (
-                  <li key={color} className="p-1 rounded bg-secondary-400">
-                    {color}
-                  </li>
-                ))}
-              </ul>
+              <>
+                <p className="text-xl text-center">Colors</p>
+                <hr className="mb-1"></hr>
+                <ul className="grid max-w-md grid-flow-col p-2 m-auto rounded bg-secondary-500 justify-items-center">
+                  {cat?.colors?.map((color) => (
+                    <li
+                      key={color}
+                      className="p-1 rounded bg-secondary-400"
+                      style={getColor(color)}
+                    >
+                      {color}
+                    </li>
+                  ))}
+                </ul>
+              </>
             )}
           </>
         ) : (
