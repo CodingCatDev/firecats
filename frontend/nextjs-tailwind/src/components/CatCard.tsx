8cc6a7a8dbf4803e43a5cff1e9ddb74e1a215847
Alex Patterson <alex@ajonp.com>
2021-10-07 13:46:34 -0400

    fix card when missing name

--- frontend/nextjs-tailwind/src/components/CatCard.tsx
+++ frontend/nextjs-tailwind/src/components/CatCard.tsx
@@ -4,15 +4,23 @@ const CatCard = ({ cat }: { cat: Cat }): JSX.Element => {
   return (
     <>
       <div className="p-2 rounded bg-primary-500 text-basics-50">
-        <h2>Name: {cat?.name}</h2>
-        <p>Name: {cat?.type}</p>
-        <ul className="grid grid-flow-col p-2 rounded bg-secondary-500 justify-items-center">
-          {cat?.colors?.map((color) => (
-            <li key={color} className="p-1 rounded bg-secondary-400">
-              {color}
-            </li>
-          ))}
-        </ul>
+        {cat.name ? (
+          <>
+            <h2>Name: {cat?.name}</h2>
+            {cat.type && <p>Type: {cat?.type}</p>}
+            {cat.colors && (
+              <ul className="grid grid-flow-col p-2 rounded bg-secondary-500 justify-items-center">
+                {cat?.colors?.map((color) => (
+                  <li key={color} className="p-1 rounded bg-secondary-400">
+                    {color}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </>
+        ) : (
+          <>You Should really name your cat!</>
+        )}
       </div>
     </>
   );
