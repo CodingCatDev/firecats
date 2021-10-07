53074f6176d3c366b0df42aa3fc4ee16b75b2966
Alex Patterson <alex@ajonp.com>
2021-10-07 13:22:53 -0400

    add firestore check with emulation

--- frontend/nextjs-tailwind/src/components/firebase/wrappers.tsx
+++ frontend/nextjs-tailwind/src/components/firebase/wrappers.tsx
@@ -1,12 +1,20 @@
-import { getAuth } from '@firebase/auth';
-import { getFirestore } from '@firebase/firestore';
+import { connectAuthEmulator, getAuth } from '@firebase/auth';
+import {
+  connectFirestoreEmulator,
+  Firestore,
+  FirestoreSettings,
+  getFirestore,
+} from '@firebase/firestore';
 import {
   useFirebaseApp,
   AuthProvider,
   FirestoreProvider,
   FirebaseAppProvider,
 } from 'reactfire';
-import { config } from '@/config/firebase';
+import { config, emulation } from '@/config/firebase';
+interface FirestoreExt extends Firestore {
+  _settings: FirestoreSettings;
+}
 
 export const FirebaseProvider = ({ children }: { children: JSX.Element }) => {
   return (
@@ -23,6 +31,9 @@ export const FirebaseAuthProvider = ({
 }) => {
   const app = useFirebaseApp();
   const auth = getAuth(app);
+  if (emulation.authEmulatorHost && !auth.emulatorConfig) {
+    connectAuthEmulator(auth, emulation.authEmulatorHost);
+  }
   return <AuthProvider sdk={auth}>{children}</AuthProvider>;
 };
 
@@ -32,6 +43,29 @@ export const FirebaseFirestoreProvider = ({
   children: JSX.Element;
 }) => {
   const app = useFirebaseApp();
-  const firestore = getFirestore(app);
+  const firestore = getFirestore(app) as FirestoreExt;
+  if (
+    emulation.firestoreEmulatorHost &&
+    firestore?._settings?.host != emulation.firestoreEmulatorHost
+  ) {
+    const { urn, port } = hostSplitter(emulation.firestoreEmulatorHost);
+    if (urn && port) {
+      connectFirestoreEmulator(firestore, urn, port);
+    }
+  }
   return <FirestoreProvider sdk={firestore}>{children}</FirestoreProvider>;
+};
+
+const hostSplitter = (host: string) => {
+  const split = host.split(':');
+  let urn = '';
+  let port = 0;
+  if (split && split.length == 2) {
+    urn = split[0];
+    port = parseInt(split[1]);
+    if (isNaN(port)) {
+      port = 0;
+    }
+  }
+  return { urn, port };
 };
