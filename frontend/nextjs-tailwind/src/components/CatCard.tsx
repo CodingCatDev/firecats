c776d67250bd56384912888463b3ecc15ebf275c
Alex Patterson <alex@ajonp.com>
2021-10-07 04:03:16 -0400

    removed unneeded firestore

--- frontend/nextjs-tailwind/src/components/CatCard.tsx
+++ frontend/nextjs-tailwind/src/components/CatCard.tsx
@@ -1,15 +1,14 @@
-import 'firebase/firestore';
 import { Cat } from '@/models/cat.model';
 
 const CatCard = ({ cat }: { cat: Cat }): JSX.Element => {
   return (
     <>
-      <div className="rounded p-2 bg-primary-500 text-basics-50">
+      <div className="p-2 rounded bg-primary-500 text-basics-50">
         <h2>Name: {cat.name}</h2>
         <p>Name: {cat.type}</p>
-        <ul className="grid grid-flow-col bg-secondary-500 rounded p-2 justify-items-center">
+        <ul className="grid grid-flow-col p-2 rounded bg-secondary-500 justify-items-center">
           {cat.colors.map((color) => (
-            <li key={color} className="bg-secondary-400 rounded p-1">
+            <li key={color} className="p-1 rounded bg-secondary-400">
               {color}
             </li>
           ))}
