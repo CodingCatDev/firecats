54a87dec5d87039bd0ca57bb01221e5a72bba47e
Alex Patterson <alex@ajonp.com>
2021-10-16 16:18:57 -0400

    add delet button

--- frontend/nextjs-tailwind/src/components/ClientSide.tsx
+++ frontend/nextjs-tailwind/src/components/ClientSide.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useFirestore, useFirestoreCollectionData, useUser } from 'reactfire';
-import { collection } from 'firebase/firestore';
+import { collection, deleteDoc, doc, orderBy, query } from 'firebase/firestore';
 
 import CatFormInput from '@/components/CatFormInput';
 
@@ -10,9 +10,16 @@ const ClientSide = (): JSX.Element => {
   const { data: user } = useUser();
   const catsRef = collection(firestore, 'cats');
 
-  const { data, status } = useFirestoreCollectionData(catsRef, {
-    idField: 'id',
-  });
+  const { data, status } = useFirestoreCollectionData(
+    query(catsRef, orderBy('name', 'asc')),
+    {
+      idField: 'id',
+    }
+  );
+
+  const onDelete = async (id: string) => {
+    await deleteDoc(doc(firestore, 'cats', id));
+  };
 
   // easily check the loading status
   if (status === 'loading') {
@@ -42,7 +49,28 @@ const ClientSide = (): JSX.Element => {
             >
               <td>
                 <Link href={`static/${d.id}`}>
-                  <a className="underline cursor-pointer">{d.name}</a>
+                  <a className="flex justify-between ">
+                    <div className="underline cursor-pointer">{d.name}</div>
+                    <button
+                      onClick={() => onDelete(d.id)}
+                      className="bg-white rounded text-error-500 hover:bg-error-500 hover:text-white"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                        />
+                      </svg>
+                    </button>
+                  </a>
                 </Link>
               </td>
               <td>{d?.type}</td>
