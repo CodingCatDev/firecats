44631080174ce6f7be8ed1abca9fd9070474e78b
Alex Patterson <alex@ajonp.com>
2021-10-18 20:48:32 -0400

    remove duplicate firestore

--- frontend/nextjs-tailwind/src/pages/clientside.tsx
+++ frontend/nextjs-tailwind/src/pages/clientside.tsx
@@ -1,24 +1,10 @@
 import dynamic from 'next/dynamic';
 
-const FirebaseFirestoreProvider = dynamic<any>(
-  () =>
-    import('@/components/firebase/wrappers').then(
-      (mod) => mod.FirebaseFirestoreProvider
-    ),
-  {
-    ssr: false,
-  }
-);
-
 const CatAdmin = dynamic<any>(() => import('@/components/CatAdmin'), {
   ssr: false,
 });
 
 const ClientSidePage = (): JSX.Element => {
-  return (
-    <FirebaseFirestoreProvider>
-      <CatAdmin />
-    </FirebaseFirestoreProvider>
-  );
+  return <CatAdmin />;
 };
 export default ClientSidePage;
