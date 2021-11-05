f2e24107a50a3084d8339b7caa1c987d7d2d66b4
Alex Patterson <alex@ajonp.com>
2021-11-05 09:16:30 -0400

    change hello api to something cool

--- frontend/nextjs-tailwind/src/pages/api/hello.ts
+++ frontend/nextjs-tailwind/src/pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  req.query?.name;
+  res.status(200).json({
+    name: `Hello ${req.query?.name ? req.query?.name : 'Alex Patterson'}`,
+  });
 }
